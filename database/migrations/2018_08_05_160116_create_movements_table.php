b4fce1882e0f99e00c3958f67b1eb1ac4a154f49
Erick Mugisha <erick.karugendo@gmail.com>
2018-08-05 19:24:31 +0300

    Add implementation to process data files

---
+++ database/migrations/2018_08_05_160116_create_movements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('FlightID', 10);
+            $table->string('RegistrationMark', 10);
+            $table->string('AircraftType', 7);
+            $table->string('AerodromeDeparture', 4);
+            $table->string('AerodromeDestination', 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movements');
+    }
+}
